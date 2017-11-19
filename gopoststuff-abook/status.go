1f0bd031e93e52c3a3b0097da4139fb312442747
Shrek Is Love <shrek@shrekislove.gq>
2017-11-18 21:02:16 -0500

    initial commit

--- gopoststuff-abook/status.go
+++ gopoststuff-abook/status.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/ShrekIsLoveLife/Audiobook-Converter/gopoststuff-abook/simplenntp"
+	"./simplenntp"
 	"time"
 )
 
--- upload.sh
+++ upload.sh
@@ -31,7 +31,7 @@ par2 c -r15 -l -a "${post_name}" *.rar
 cksfv *.rar > "${post_name}.sfv"
 
 cd ../
-${DIR}/gopoststuff-abook -g 'alt.binaries.mp3.abooks' -nzb "${post_name}.nzb" -rarpw "${post_name}" -d "${post_name}"
+${DIR}/gopoststuff-abook/gopoststuff-abook -g 'alt.binaries.mp3.abooks' -nzb "${post_name}.nzb" -rarpw "${post_name}" -d "${post_name}"
 echo 
 echo wait \(15 or so min\) for it to show up on:
 echo "https://nzbindex.com/search/?q=${post_name}"
